4268e9a966bff1d3a9252f5732cce65e95f53934
Guilherme Nass <nassguilherme@gmail.com>
2018-10-17 01:48:12 -0300

    ajustes para exportar rota de e-mails

--- routes/email-routes.js
+++ routes/email-routes.js
@@ -5,3 +5,5 @@ var emailController = require('../controllers/EmailController');
 router.post('/success', function(req, res){
     new emailController(req, res).sendEmailSuccessfulEvaluation(this.req.body.email);
 })
+
+module.exports = router;
