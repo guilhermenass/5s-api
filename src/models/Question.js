e6f2381c903daa2ea47e68b8ee7fd1a996587c6c
guilhermenass <nassguilherme@gmail.com>
2018-11-14 22:06:16 -0200

    migrations + documentação + ajustes configuraçao de model

--- src/models/Question.js
+++ src/models/Question.js
@@ -1,27 +1,48 @@
 module.exports = (sequelize, DataTypes) => {  
     
 	var Question = sequelize.define('Question', {
+
+		/**
+		 * Identificador da pergunta
+		 */
 		id: {
 			primaryKey: true,
 			type: DataTypes.INTEGER,
 			autoIncrement: true
 		},
+
+		/**
+		 * Título da pergunta
+		 */
 		title: {
 			type: DataTypes.STRING
 		},
+
+		/**
+		 * Senso em que a pergunta se adequa
+		 */
 		sense: {
 			type: DataTypes.INTEGER
 		},
+
+		/**
+		 * Descrição da pergunta
+		 */
 		description: {
 			type: DataTypes.STRING
+		},
+
+		/**
+		 * Verifica se o registro está ativo (técnica de exclusão lógica)
+		 */
+		is_active: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: true
 		}
 	},
 	{
 		tableName: 'questions',
 		timestamps: false, /* false para não criar colunas createdAt e updateAt no banco */
 	})
-    
-
-
 	return Question
 }
